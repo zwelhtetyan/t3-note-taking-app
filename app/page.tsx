a84ddbd387e92bfdf062d753e80e4bf35fdf638e
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-07 11:03:38 +0630

    get data from postgresql using prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+interface Post {
+  id: number;
+  title: string;
+  content?: string;
+  publish?: boolean;
+}
+
+const getPosts = async (): Promise<Post[]> => {
+  const res = await fetch(`${process.env.BASE_URL}/api/post`);
+  const posts = await res.json();
+  return posts;
+};
+
+export default async function Home() {
+  const allPosts = await getPosts();
+
   return (
     <main>
-      <h1 className='text-gray-700 text-xl'>Hello from home page</h1>
+      <h1 className='text-lg mb-5'>Hello from home page</h1>
+
+      {allPosts.map(({ id, title }) => (
+        <h2 key={id} className='text-blue-700'>
+          {title}
+        </h2>
+      ))}
     </main>
   );
 }
