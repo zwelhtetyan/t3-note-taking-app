160a5f049edfa5b04e3f918b7a9761d4035a20d3
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-12 11:05:31 +0630

    update:

--- lib/prismaClient.ts
+++ lib/prismaClient.ts
@@ -1,15 +1,8 @@
-// import { PrismaClient } from '@prisma/client';
-
-// export const prisma = new PrismaClient();
-
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
