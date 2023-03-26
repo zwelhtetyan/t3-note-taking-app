07e813d7097c064502d222055b90602836e0d6bb
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-26 22:46:16 +0630

    revalidate on every 3s

--- src/hooks/useGetUsers.tsx
+++ src/hooks/useGetUsers.tsx
@@ -5,7 +5,9 @@ import type { Post } from "~/types";
 
 const useGetUsers = () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { data, isLoading, error, mutate } = useSWR("/posts", fetcher);
+  const { data, isLoading, error, mutate } = useSWR("/posts", fetcher, {
+    refreshInterval: 3000,
+  });
 
   return {
     data: data as Post[],
