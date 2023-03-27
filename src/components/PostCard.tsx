9932977daa464ef839624b3803960238aa8511ab
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-27 17:10:57 +0630

    add ManageDropdown component

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,17 +1,48 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Moment from "react-moment";
 import defaultAvatar from "~/assets/images/default_profile.webp";
 import useGetPosts from "~/hooks/useGetPosts";
 import { axiosInstance } from "~/lib/axiosInstance";
 import type { Post } from "~/types";
 
+const ManageDropdown = ({
+  handleDeletePost,
+  deleting,
+}: {
+  handleDeletePost: () => void;
+  deleting: boolean;
+}) => {
+  return (
+    <ul
+      tabIndex={0}
+      className="dropdown-content menu rounded-box absolute top-[35px] right-0 z-10 w-52 border bg-base-100 p-2 shadow dark:border-gray-700"
+    >
+      <li>
+        <a>Edit</a>
+      </li>
+      <li onClick={handleDeletePost}>
+        <a>{deleting ? "Deleting" : "Delete"}</a>
+      </li>
+    </ul>
+  );
+};
+
 const PostCard = ({ ...post }: Post) => {
+  const { data } = useSession();
   const { mutate: mutatePosts } = useGetPosts();
+  const [showDropdown, setShowDropdown] = useState(false);
   const [deleting, setDeleting] = useState(false);
   const [editing, setEditing] = useState(false);
-  const { data } = useSession();
+
+  const toggleDropdown = () => {
+    setShowDropdown((prev) => !prev);
+  };
+
+  const closeDropdown = () => {
+    setShowDropdown(false);
+  };
 
   const isAuthor = useMemo(
     () => data?.user?.id === post.author?.id,
@@ -33,6 +64,7 @@ const PostCard = ({ ...post }: Post) => {
     }
 
     setDeleting(false);
+    closeDropdown();
   };
 
   // const handleEditPost = async (postId: string, userId: string | undefined) => {
@@ -51,7 +83,7 @@ const PostCard = ({ ...post }: Post) => {
 
   return (
     <div className="mb-5 border-b pb-5 dark:border-gray-700">
-      <header className="flex justify-between">
+      <header className="relative flex justify-between">
         <div className="flex items-center">
           <Image
             width={100}
@@ -70,27 +102,47 @@ const PostCard = ({ ...post }: Post) => {
         </div>
 
         {isAuthor && (
-          <div className="dropdown-left dropdown">
-            <button className="btn-outline btn h-8 min-h-0 border-gray-300 p-2 text-neutral-500 hover:bg-neutral-500 dark:border-gray-600 dark:hover:bg-gray-500">
-              •••
-            </button>
+          <button
+            onClick={toggleDropdown}
+            className="btn-outline btn h-8 min-h-0 border-gray-300 p-2 text-neutral-500 hover:bg-neutral-500 dark:border-gray-600 dark:hover:bg-gray-500"
+          >
+            •••
+          </button>
+        )}
 
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu rounded-box w-52 border bg-base-100 p-2 shadow dark:border-gray-700"
-            >
-              <li>
-                <a>Edit</a>
-              </li>
-              <li onClick={() => handleDeletePost(post.id, post.author?.id)}>
-                <a>{deleting ? "Deleting" : "Delete"}</a>
-              </li>
-            </ul>
-          </div>
+        {showDropdown && (
+          <ManageDropdown
+            handleDeletePost={() => handleDeletePost(post.id, post.author?.id)}
+            deleting={deleting}
+          />
         )}
       </header>
 
       <div className="mt-2 ml-12">{post.content}</div>
+
+      {/* The button to open modal */}
+      <label htmlFor="my-modal" className="btn">
+        open modal
+      </label>
+
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">
+            Congratulations random Internet user!
+          </h3>
+          <p className="py-4">
+            You have been selected for a chance to get one year of subscription
+            to use Wikipedia for free!
+          </p>
+          <div className="modal-action">
+            <label htmlFor="my-modal" className="btn">
+              Yay!
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
