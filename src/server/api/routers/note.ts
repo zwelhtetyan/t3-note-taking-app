ac06149983763ed2307c84c82fd5b987489212a2
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-04-12 22:44:01 +0630

    test: rate_limit (2, 1m)

--- src/server/api/routers/note.ts
+++ src/server/api/routers/note.ts
@@ -7,7 +7,7 @@ import { Redis } from "@upstash/redis";
 // Create a new ratelimiter, that allows 10 requests per 10 seconds
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  limiter: Ratelimit.slidingWindow(2, "1 m"),
   analytics: true,
 });
 
